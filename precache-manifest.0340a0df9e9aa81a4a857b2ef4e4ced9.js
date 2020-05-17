3ae2394210a132c8aebe88febbeb03827fc7b44d
endy30 <hlamah@connect.ust.hk>
2020-05-17 19:06:45 +0800

    Updates

---
+++ precache-manifest.0340a0df9e9aa81a4a857b2ef4e4ced9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bffe59c2c316cfcd7186b3197628e7b4",
+    "url": "/MyMonster-react/index.html"
+  },
+  {
+    "revision": "42fe8edcd85b871e4a8d",
+    "url": "/MyMonster-react/static/css/main.c9ddd7e7.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/MyMonster-react/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyMonster-react/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42fe8edcd85b871e4a8d",
+    "url": "/MyMonster-react/static/js/main.45d7975c.chunk.js"
+  },
+  {
+    "revision": "214dd6915559fe5ef860",
+    "url": "/MyMonster-react/static/js/runtime-main.37e66f10.js"
+  }
+]);
